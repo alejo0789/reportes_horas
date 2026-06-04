2af19c9fda2e1ef0dd28c52405b6ca2a322a0328
Alejandro Carvajal Gonzalez <alejandro.carvajal@LAFORTUNA.LOCAL>
2026-06-04 16:42:55 -0500

    added PORT NUMBRE

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     logger.info("Starting Sales per Hour Dashboard Server...")
     # Run the uvicorn development server
     # Will serve API routes at /api/* and frontend static files at root /
-    uvicorn.run("backend.main:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("backend.main:app", host="127.0.0.1", port=8032, reload=True)
